f00c2b6ac4d3a0522882f5292b8c86cb4f0ec8cc
kyh888888884 <k37237280@gmail.com>
2022-08-24 09:00:41 +0900

    morgan

--- app/app.js
+++ app/app.js
@@ -1,20 +1,27 @@
 'use strict';
 
 const express = require("express");
-const app = express();
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+const app = express();
 dotenv.config();
 
 // setting app
 app.set("views", "./src/views");
 app.set("view engine", "ejs"); // ejs is templete view engine; embedded javascript template.
 
-// middleware
+// routing
 const home = require("./src/routes/home"); // import index.js -> route
+
+const accessLogStream = require("./src/config/log");
+
+// middleware
 app.use(express.static(`${__dirname}/src/public`)) // it gives other files access to public directory.
 app.use(express.json()); // body-parser.
 app.use(express.urlencoded({ extended: true})); // to solve the url problem.
+app.use(morgan("common", { stream: accessLogStream })); // log sign in/out
 
-app.use("/", home); // it must be executed at last.
+app.use("/", home); // register middlewares.
 
 module.exports = app;
